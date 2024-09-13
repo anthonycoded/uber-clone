1167f12c5df7bf4928e9624a3a93d3194a2f18b5
Shawn <barbels33@gmail.com>
2024-09-12 22:50:20 -0400

    created oAuth component

---
+++ components/OAuth.tsx
@@ -0,0 +1,38 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import CustomBtn from "./CustomBtn";
+import { icons } from "@/constants";
+
+type Props = {};
+
+const OAuth = (props: Props) => {
+  const handleGoogleSignIn = async () => {};
+  return (
+    <View className="flex-1 items-center w-full">
+      <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
+        <View className="flex-1 h-[1px] bg-general-100" />
+        <Text className="text-lg">Or</Text>
+        <View className="flex-1 h-[1px] bg-general-100" />
+      </View>
+
+      <CustomBtn
+        title="Login with Google"
+        className="mt-5 w-full shadow-none"
+        onPress={handleGoogleSignIn}
+        IconLeft={() => (
+          <Image
+            source={icons.google}
+            resizeMode="contain"
+            className="w-5 h-5 mx-2"
+          />
+        )}
+        bgVariant="outline"
+        textVariant="primary"
+      />
+    </View>
+  );
+};
+
+export default OAuth;
+
+const styles = StyleSheet.create({});
