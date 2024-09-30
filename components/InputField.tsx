bb83508bd0dba1d174f98b96161b0082d2ea687b
Shawn <barbels33@gmail.com>
2024-09-30 12:24:48 -0400

    updated styles

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -32,18 +32,19 @@ const InputField = ({
       behavior={Platform.OS === "ios" ? "padding" : "height"}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View className="my-2 w-full">
+        <View className="my-3 w-full">
           <Text className={`text-lg font-JakartaSemiBold ${labelStyle}`}>
             {label}
           </Text>
 
           <View
-            className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
+            className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-xl border border-neutral-100 focus:border-primary-500 ${containerStyle}`}
           >
             {icon && (
               <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
             )}
             <TextInput
+              onChangeText={onChangeText}
               className={`rounded-full p-4 font-JakartaSemiBold bg-gray-100 text-[15px] flex-1 text-left ${inputStyle}`}
               secureTextEntry={secureTextEntry}
               placeholder={placeHolder}
--- components/OAuth.tsx
+++ components/OAuth.tsx
@@ -8,7 +8,7 @@ type Props = {};
 const OAuth = (props: Props) => {
   const handleGoogleSignIn = async () => {};
   return (
-    <View className="flex-1 items-center w-full">
+    <View className="flex-1 items-center w-full mb-4">
       <View className="flex flex-row justify-center items-center mt-4 gap-x-3">
         <View className="flex-1 h-[1px] bg-general-100" />
         <Text className="text-lg">Or</Text>
