44548c7b4c4902115bcac254c9743c57d476e052
Shawn <barbels33@gmail.com>
2024-09-30 12:23:00 -0400

    fixed bug

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -12,8 +12,8 @@ const Onboarding = (props: Props) => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const isLastSlide = activeIndex === onboarding.length;
-  -1;
+  const isLastSlide = activeIndex === onboarding.length - 1;
+
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
